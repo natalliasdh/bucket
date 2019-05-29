756c4813cf3a9231f4852487a4f0b9c7786bdb15
Brandon Blanks <brandoncblanks@gmail.com>
2019-05-29 13:11:32 -0400

    initial commit of server and json

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
         }
     });
     User.associate = function(models) {
-        User.hasMany(models.bucketList, {
+        User.hasMany(models.BucketList, {
             onDelete: "cascade"
         });
     };
