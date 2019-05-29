9bd3679c9961a3b56f581875ef9beb35f4a7a75e
enzofass <zoefassioli@gmail.com>
2019-05-29 12:55:35 -0400

    updated user model

--- models/user.js
+++ models/user.js
@@ -1,24 +1,36 @@
 module.exports = function(sequelize, DataTypes) {
-    const User = sequelize.define("User", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false 
-        },
-
-        location: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-
-        progress: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0
-        }
+  const User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: 8
+      }
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    progress: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    }
+  });
+  User.associate = function(models) {
+    User.hasMany(models.bucketList, {
+      onDelete: "cascade"
     });
-    User.associate = function(models) {
-        User.hasMany(models.bucketList, {
-            onDelete: "cascade"
-        });
-    };
-    return User;
+  };
+  return User;
 };
