12a8541d2b42ac1b7fae8fcf6dec71b3b4e6383c
natalliasdh <natallia.sdh@gmail.com>
2019-05-31 09:39:36 -0400

    redirect, local storage

---
+++ public/js/profile.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+
+    
+                var n=localStorage.getItem("ID");
+            $("#hello").append("helllllooooooWS");
+            console.log("hello");
+            console.log(n);
+      
+});
